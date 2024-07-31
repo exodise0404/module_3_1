bb8ec40bae7c846206b6df388fa833a8cfc9a36e
exodise0404 <exodise0404@gmail.com>
2024-07-31 21:24:20 +0800

    Add files via upload

--- module_3_1.py
+++ module_3_1.py
@@ -6,25 +6,22 @@ def count_calls():
 
 
 def string_info(string):
+    count_calls()
     return (len(string), string.upper(), string.lower())
-
 
 print(string_info('Capybara'))
 print(string_info('Armageddon'))
 
-count_calls()
 
 def is_contains(string, list_to_search):
-    list_to_search = []
-    if string in list_to_search:
+    count_calls()
+    if str.lower(string) in (item.lower() for item in list_to_search):
         return True
     else:
         return False
 
-
 print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
 print(is_contains('cycle', ['recycling', 'cyclic']))
 
-count_calls()
 
 print(calls)
